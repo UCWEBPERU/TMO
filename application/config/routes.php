12202bea58634d5a7168e2e34e2be7c29505e501
Mick Heral V.E <mickhve@gmail.com>
2016-01-14 17:53:55 -0500

    updates

--- application/config/routes.php
+++ application/config/routes.php
@@ -69,6 +69,8 @@ $route['admin/empresa/crear']		= 'admin/module/empresa/C_Admin_Empresa/insert';
 $route['admin/empresa/editar']		= 'admin/module/empresa/C_Admin_Empresa/update';
 $route['admin/empresa/eliminar']	= 'admin/module/empresa/C_Admin_Empresa/delete';
 
+$route['admin/perfil']				= 'admin/module/perfil/C_Admin_Perfil';
+
 
 /*
 | ------------------------------
---
+++ application/controllers/admin/module/perfil/C_Admin_Perfil.php
@@ -0,0 +1,40 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class C_Admin_Perfil extends CI_Controller {
+
+	public function __construct() {
+		parent::__construct();
+		$this->load->helper('url');
+		$this->load->library('session');
+        $this->load->library('utils/UserSession');
+        
+        $this->usersession->loadSession($this->session);
+        
+        if (!$this->usersession->validateSession()) {
+            redirect("/admin/login");
+        } else {
+            if ($this->usersession->validateSession() == 2) {
+                redirect("/store/".$this->uri->segment(2)."/admin");
+            }
+        }
+		
+	}
+
+	public function index()	{
+        $modulo = new stdClass();
+        $modulo->titulo_pagina = "TMO | Panel Principal - Perfil";
+        
+        $modulo->icono_empresa = PATH_RESOURCE_ADMIN."img/icon/icon_app.png";
+        $modulo->nombres_usuario = "Nombres";
+        $modulo->tipo_usuario = "Super Administrador";
+        $modulo->nombre_empresa_largo = "Take My Order";
+        $modulo->nombre_empresa_corto = "TMO";
+        
+        $modulo->url_signout = base_url()."admin/signOut";
+        
+        $data["modulo"] = $modulo;
+        $this->load->view('admin/module/perfil/v-admin-perfil', $data);
+	}
+
+}
