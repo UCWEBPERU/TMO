2b8bcf2c5e24e32dbbc85a3162e8847b8443a97a
Mick Heral V.E <mickhve@gmail.com>
2016-01-15 17:05:31 -0500

    updates

--- application/views/admin/module/perfil/v-admin-perfil.php
+++ application/views/admin/module/perfil/v-admin-perfil.php
@@ -40,23 +40,23 @@
                     <div class="box-body">
                         <div class="form-group">
                         <label for="txtNombres">Nombres</label>
-                        <input type="text" class="form-control" id="txtNombres" name="txtNombres" value="<?php echo $modulo->datos_usuario->nombres_persona; ?>" data-parsley-required data-parsley-type="alphanum" data-parsley-required-message="Ingrese nombre."/>
+                        <input type="text" class="form-control" id="txtNombres" name="txtNombres" value="<?php echo $modulo->datos_usuario->nombres_persona; ?>" data-parsley-required data-parsley-required-message="Ingrese nombre."/>
                         </div>
                         <div class="form-group">
                         <label for="txtApellidos">Apellidos</label>
-                        <input type="text" class="form-control" id="txtApellidos" name="txtApellidos" value="<?php echo $modulo->datos_usuario->apellidos_persona; ?>" data-parsley-required data-parsley-type="alphanum" data-parsley-required-message="Ingrese apellido."/>
+                        <input type="text" class="form-control" id="txtApellidos" name="txtApellidos" value="<?php echo $modulo->datos_usuario->apellidos_persona; ?>" data-parsley-required data-parsley-required-message="Ingrese apellido."/>
                         </div>
                         <div class="form-group">
                         <label for="txtPais">Pais</label>
-                        <input type="text" class="form-control" id="txtPais" name="txtPais" value="<?php echo $modulo->datos_usuario->pais_region_persona; ?>" data-parsley-required data-parsley-type="alphanum" data-parsley-required-message="Ingrese su pais."/>
+                        <input type="text" class="form-control" id="txtPais" name="txtPais" value="<?php echo $modulo->datos_usuario->pais_region_persona; ?>" data-parsley-required data-parsley-required-message="Ingrese su pais."/>
                         </div>
                         <div class="form-group">
                         <label for="txtEstado">Estado</label>
-                        <input type="text" class="form-control" id="txtEstado" name="txtEstado" value="<?php echo $modulo->datos_usuario->estado_region_persona; ?>" data-parsley-required data-parsley-type="alphanum" data-parsley-required-message="Ingrese su estado."/>
+                        <input type="text" class="form-control" id="txtEstado" name="txtEstado" value="<?php echo $modulo->datos_usuario->estado_region_persona; ?>" data-parsley-required data-parsley-required-message="Ingrese su estado."/>
                         </div>
                         <div class="form-group">
                         <label for="txtDireccion">Direccion</label>
-                        <input type="text" class="form-control" id="txtDireccion" name="txtDireccion" value="<?php echo $modulo->datos_usuario->direccion_persona; ?>" data-parsley-required data-parsley-type="alphanum" data-parsley-required-message="Ingrese su direccion."/>
+                        <input type="text" class="form-control" id="txtDireccion" name="txtDireccion" value="<?php echo $modulo->datos_usuario->direccion_persona; ?>" data-parsley-required data-parsley-required-message="Ingrese su direccion."/>
                         </div>
                         <div class="form-group">
                         <label for="txtNumeroCelular">Numero Celular</label>
