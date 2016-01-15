ba310d11c9174cadecc499f69f91ed3655cfbfcc
Mick Heral V.E <mickhve@gmail.com>
2016-01-15 09:38:26 -0500

    updates

--- application/views/template/main-panel/footer.php
+++ application/views/template/main-panel/footer.php
@@ -2,7 +2,7 @@
 <footer class="main-footer">
 	<!-- To the right -->
 	<div class="pull-right hidden-xs">
-		<strong>Version</strong> 0.0 | <strong>Desarrollador</strong> <a href="http://www.uc-web.com/" target="_blank">UC-WEB</a>
+		<strong>Version</strong> 0.0 | <strong>Desarrollador</strong> <a href="http://www.gotmo.net" target="_blank">GOTMO</a>
 	</div>
 	<!-- Default to the left -->
 	<strong>Copyright &copy; 2015 <a href="<?php echo base_url(); ?>" target="_blank">TMO</a>.</strong> Todos los derechos reservados.
