6fcfdf3720cb97b56039cda9d9558b03aaab5c52
Mick Heral V.E <mickhve@gmail.com>
2016-01-15 10:20:59 -0500

    updates

--- application/views/admin/module/perfil/v-admin-perfil.php
+++ application/views/admin/module/perfil/v-admin-perfil.php
@@ -28,54 +28,84 @@
 
           <div class="row">
             <div class="col-md-6">
-          <!-- general form elements -->
-          <div class="box box-primary">
-            <div class="box-header with-border">
-              <h3 class="box-title">Mi Perfil</h3>
+            <!-- general form elements -->
+            <div class="box box-primary">
+                <div class="box-header with-border">
+                <h3 class="box-title">Datos Personales</h3>
+                </div>
+                <!-- /.box-header -->
+                <!-- form start -->
+                <form role="form">
+                <div class="box-body">
+                    <div class="form-group">
+                    <label for="txtNombres">Nombres</label>
+                    <input type="email" class="form-control" id="txtNombres" />
+                    </div>
+                    <div class="form-group">
+                    <label for="txtApellidos">Apellidos</label>
+                    <input type="email" class="form-control" id="txtApellidos" />
+                    </div>
+                    <div class="form-group">
+                    <label for="txtPais">Pais</label>
+                    <input type="email" class="form-control" id="txtPais" />
+                    </div>
+                    <div class="form-group">
+                    <label for="txtEstado">Estado</label>
+                    <input type="email" class="form-control" id="txtEstado" />
+                    </div>
+                    <div class="form-group">
+                    <label for="txtDireccion">Direccion</label>
+                    <input type="email" class="form-control" id="txtDireccion" />
+                    </div>
+                    <div class="form-group">
+                    <label for="txtNumeroCelular">Numero Celular</label>
+                    <input type="email" class="form-control" id="txtNumeroCelular" />
+                    </div>
+                    <div class="form-group">
+                    <label for="txtNumeroTelefonico">Numero Telefonico</label>
+                    <input type="email" class="form-control" id="txtNumeroTelefonico" />
+                    </div>
+                </div>
+                <!-- /.box-body -->
+
+                <div class="box-footer">
+                    <button type="submit" class="btn btn-primary">Guardar</button>
+                </div>
+                </form>
             </div>
-            <!-- /.box-header -->
-            <!-- form start -->
-            <form role="form">
-              <div class="box-body">
-                <div class="form-group">
-                  <label for="exampleInputEmail1">Nombres</label>
-                  <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
-                </div>
-                <div class="form-group">
-                  <label for="exampleInputEmail1">Apellidos</label>
-                  <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
-                </div>
-                <div class="form-group">
-                  <label for="exampleInputEmail1">Pais</label>
-                  <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
-                </div>
-                <div class="form-group">
-                  <label for="exampleInputEmail1">Estado</label>
-                  <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
-                </div>
-                <div class="form-group">
-                  <label for="exampleInputEmail1">Direccion</label>
-                  <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
-                </div>
-                <div class="form-group">
-                  <label for="exampleInputEmail1">Numero Celular</label>
-                  <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
-                </div>
-                <div class="form-group">
-                  <label for="exampleInputEmail1">Numero Telefonico</label>
-                  <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
-                </div>
-              </div>
-              <!-- /.box-body -->
+            <!-- /.box  -->
 
-              <div class="box-footer">
-                <button type="submit" class="btn btn-primary">Guardar</button>
-              </div>
-            </form>
-          </div>
-          <!-- /.box  -->
+            </div>
+            
+            <div class="col-md-6">
+            <!-- general form elements -->
+            <div class="box box-primary">
+                <div class="box-header with-border">
+                <h3 class="box-title">Cuenta de Usuario</h3>
+                </div>
+                <!-- /.box-header -->
+                <!-- form start -->
+                <form role="form">
+                <div class="box-body">
+                    <div class="form-group">
+                        <label for="txtNombreUsuario">Nombre Usuario</label>
+                        <input type="email" class="form-control" id="txtNombreUsuario" />
+                    </div>
+                    <div class="form-group">
+                        <label for="txtPassword">Password</label>
+                        <input type="email" class="form-control" id="txtPassword" />
+                    </div>
+                </div>
+                <!-- /.box-body -->
 
-          </div>
+                <div class="box-footer">
+                    <button type="submit" class="btn btn-primary">Guardar</button>
+                </div>
+                </form>
+            </div>
+            <!-- /.box  -->
+
+            </div>
 
           </div><!-- /.row -->
           
