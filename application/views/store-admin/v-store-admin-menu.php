c31478a026d3d7a48a1625081a5553a705f9cb3d
Mick Heral V.E <mickhve@gmail.com>
2016-01-19 16:03:32 -0500

    updates

--- application/views/store-admin/v-store-admin-menu.php
+++ application/views/store-admin/v-store-admin-menu.php
@@ -5,10 +5,10 @@
 		<!-- Sidebar Menu -->
 		<ul class="sidebar-menu">
 			<li class="header">MENU</li>
-            <li <?php echo ($menu == 1) ? 'class="active"' : ''; ?>><a href="store/id/admin/perfil-store"><i class="fa fa-building-o"></i> <span>Empresa</span></a></li>
-            <li <?php echo ($menu == 1) ? 'class="active"' : ''; ?>><a href="store/id/admin/productos"><i class="fa fa-building-o"></i> <span>Productos</span></a></li>
-            <li <?php echo ($menu == 2) ? 'class="active"' : ''; ?>><a href="store/id/admin/categorias"><i class="fa fa-building-o"></i> <span>Categorias</span></a></li>
-            <li <?php echo ($menu == 4) ? 'class="active"' : ''; ?>><a href="store/id/admin/perfil"><i class="fa fa-user"></i> <span>Perfil</span></a></li>
+            <li <?php echo ($menu == 1) ? 'class="active"' : ''; ?>><a href="store/<?php echo $this->session->id_empresa; ?>/admin/perfil-store"><i class="fa fa-building-o"></i> <span>Empresa</span></a></li>
+            <li <?php echo ($menu == 1) ? 'class="active"' : ''; ?>><a href="store/<?php echo $this->session->id_empresa; ?>/admin/productos"><i class="fa fa-building-o"></i> <span>Productos</span></a></li>
+            <li <?php echo ($menu == 2) ? 'class="active"' : ''; ?>><a href="store/<?php echo $this->session->id_empresa; ?>/admin/categorias"><i class="fa fa-building-o"></i> <span>Categorias</span></a></li>
+            <li <?php echo ($menu == 4) ? 'class="active"' : ''; ?>><a href="store/<?php echo $this->session->id_empresa; ?>/admin/perfil"><i class="fa fa-user"></i> <span>Perfil</span></a></li>
 		</ul><!-- /.sidebar-menu -->
 	</section>
 	<!-- /.sidebar -->
