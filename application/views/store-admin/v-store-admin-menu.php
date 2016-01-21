919cc4a2c4619d33c4d81614062850806434cfea
Mick Heral V.E <mickhve@gmail.com>
2016-01-21 14:14:43 -0500

    updates

--- application/views/store-admin/v-store-admin-menu.php
+++ application/views/store-admin/v-store-admin-menu.php
@@ -6,8 +6,8 @@
 		<ul class="sidebar-menu">
 			<li class="header">MENU</li>
             <li <?php echo ($menu == 1) ? 'class="active"' : ''; ?>><a href="store/<?php echo $this->session->id_empresa; ?>/admin/perfil-store"><i class="fa fa-building-o"></i> <span>Empresa</span></a></li>
-            <li <?php echo ($menu == 1) ? 'class="active"' : ''; ?>><a href="store/<?php echo $this->session->id_empresa; ?>/admin/productos"><i class="fa fa-shopping-bag"></i> <span>Productos</span></a></li>
-            <li <?php echo ($menu == 2) ? 'class="active"' : ''; ?>><a href="store/<?php echo $this->session->id_empresa; ?>/admin/categorias"><i class="fa fa-tags"></i> <span>Categorias</span></a></li>
+            <li <?php echo ($menu == 2) ? 'class="active"' : ''; ?>><a href="store/<?php echo $this->session->id_empresa; ?>/admin/productos"><i class="fa fa-shopping-bag"></i> <span>Productos</span></a></li>
+            <li <?php echo ($menu == 3) ? 'class="active"' : ''; ?>><a href="store/<?php echo $this->session->id_empresa; ?>/admin/categorias"><i class="fa fa-tags"></i> <span>Categorias</span></a></li>
             <li <?php echo ($menu == 4) ? 'class="active"' : ''; ?>><a href="store/<?php echo $this->session->id_empresa; ?>/admin/perfil"><i class="fa fa-user"></i> <span>Perfil</span></a></li>
 		</ul><!-- /.sidebar-menu -->
 	</section>
