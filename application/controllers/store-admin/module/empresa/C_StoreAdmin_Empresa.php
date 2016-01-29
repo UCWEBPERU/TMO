441f609c4cd45f27487f7616669b6dcc44323acd
Mick Heral V.E <mickhve@gmail.com>
2016-01-29 18:06:21 -0500

    updates

--- application/controllers/store-admin/module/empresa/C_StoreAdmin_Empresa.php
+++ application/controllers/store-admin/module/empresa/C_StoreAdmin_Empresa.php
@@ -184,8 +184,8 @@ class C_StoreAdmin_Empresa extends CI_Controller {
             
             $result = $this->M_StoreAdmin_Empresa->updateLogoOnEmpresa(
                 array(
-                    "url_archivo"   => $dataEmpresa[0]->id_archivo_logo,
-                    "path"          => $path["path"]
+                    "id_archivo"   => $dataEmpresa[0]->id_archivo_logo,
+                    "url_archivo"  => $path["path"]
                 )
             );
             
