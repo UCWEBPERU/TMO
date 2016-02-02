a830117594afc389458d59c56b3c0f8ae47860cc
Mick Heral V.E <mickhve@gmail.com>
2016-02-02 12:25:12 -0500

    updates

--- resources/admin/js/HandleFile.js
+++ resources/admin/js/HandleFile.js
@@ -46,31 +46,3 @@ HandleFile.prototype.onSelect = function(event) {
         reader.readAsDataURL(f);
     }
 };
-
-var HandleFile = {
-    onSelect: function(event) {
-        var files = evt.target.files; // FileList object
-        // Loop through the FileList and render image files as thumbnails.
-        for (var i = 0, f; f = files[i]; i++) {
-        
-            // Only process image files.
-            if (!f.type.match('image.*')) {
-                continue;
-            }
-            
-            this.formData.append(this.nameFile, f);
-        
-            var reader = new FileReader();
-        
-            // Closure to capture the file information.
-            reader.onload = (function(theFile) {
-                return function(e) {
-                    $(this.selectorImg).attr("src", e.target.result);
-                };
-            })(f);
-        
-            // Read in the image file as a data URL.
-            reader.readAsDataURL(f);
-        }
-    }
-}
