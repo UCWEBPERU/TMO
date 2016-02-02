fcac128140073afbde238367a5acdaa3062b4f26
Mick Heral V.E <mickhve@gmail.com>
2016-02-02 12:50:22 -0500

    updates

--- resources/admin/js/HandleFile.js
+++ resources/admin/js/HandleFile.js
@@ -1,13 +1,25 @@
 function HandleFile(selectorFile) {
     this.selectorFile = selectorFile;
+    this.callbackOnSuccess;
+    this.callbackOnReadFile;
 }
 
 HandleFile.prototype.listen = function(callbackOnSuccess, callbackOnReadFile) {
+    this.callbackOnSuccess = callbackOnSuccess;
+    this.callbackOnReadFile = callbackOnReadFile;
     if (typeof this.selectorFile === 'string') {
         $(this.selectorFile).on("change", this.onSelect);
     } else {
         throw "El selector debe ser del tipo cadena.";
     }
+}
+
+HandleFile.prototype.fileOnSuccess = function(file) {
+    this.callbackOnSuccess(file);
+}
+
+HandleFile.prototype.fileOnRead = function(result) {
+    this.callbackOnSuccess(result);
 }
 
 HandleFile.prototype.onSelect = function(event) {
@@ -20,7 +32,7 @@ HandleFile.prototype.onSelect = function(event) {
             continue;
         }
         
-        callbackOnSuccess(f);
+        this.fileOnSuccess(f);
         
         // this.formData.append(this.nameFile, f);
     
@@ -30,7 +42,7 @@ HandleFile.prototype.onSelect = function(event) {
         reader.onload = (function(theFile) {
             return function(e) {
                 // $(this.selectorImg).attr("src", e.target.result);
-                callbackOnReadFile(f);
+                this.fileOnRead(e.target.result);
             };
         })(f);
     
