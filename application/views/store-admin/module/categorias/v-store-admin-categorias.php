cb68bda52af8641074a5302827238ad38b7700e6
Mick Heral V.E <mickhve@gmail.com>
2016-02-04 10:59:18 -0500

    updates

--- application/views/store-admin/module/categorias/v-store-admin-categorias.php
+++ application/views/store-admin/module/categorias/v-store-admin-categorias.php
@@ -58,6 +58,20 @@
         (function($){
             $(".btn-box-tool").on("click", function(){
                 sweetAlert("Oops...", "Something went wrong!", "error");
+                swal({
+                        title: "Are you sure?",
+                        text: "You will not be able to recover this imaginary file!",
+                        type: "warning",
+                        showCancelButton: true,
+                        confirmButtonColor: "#DD6B55",
+                        confirmButtonText: "Yes, delete it!",
+                        closeOnConfirm: false,
+                        showLoaderOnConfirm: true
+                    }, 
+                    function() {
+                        swal("Deleted!", "Your imaginary file has been deleted.", "success"); 
+                    }
+                );
             });
         })(jQuery);
     </script>
