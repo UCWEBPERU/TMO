90b2b26f5729a4487934beb7ee3f5cb908a8fc56
Mick Heral V.E <mickhve@gmail.com>
2016-02-04 17:45:43 -0500

    updates

--- application/views/store-admin/module/categorias/v-store-admin-categorias-add.php
+++ application/views/store-admin/module/categorias/v-store-admin-categorias-add.php
@@ -32,7 +32,7 @@
             <div class="col-md-6">
                 <div class="box box-primary">
                     <div class="box-header with-border">
-                    <h3 class="box-title">Agregar Categoria</h3>
+                    <h3 class="box-title">Datos Categoria</h3>
                     </div>
                     <!-- /.box-header -->
                     <!-- form start -->
