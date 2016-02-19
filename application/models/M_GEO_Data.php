83041c84c5ef1a68e2e712d062a5b54ed31fb305
Mick Heral V.E <mickhve@gmail.com>
2016-02-19 16:56:22 -0500

    updates

--- application/models/M_GEO_Data.php
+++ application/models/M_GEO_Data.php
@@ -7,17 +7,20 @@ class M_GEO_Data extends CI_Model{
     }
 
     public function getAllCountries() {
+        $this->db->order_by("name", "asc");
         $query = $this->db->get('GEO_Countries');
         return $query->result();
     }
 
     public function getRegionsByCountry($data_country) {
+        $this->db->order_by("name", "asc");
         $this->db->where('country', $data_country["code"]);
         $query = $this->db->get('GEO_Regions');
         return $query->result();
     }
 
     public function getCitiesByRegionAndCountry($data) {
+        $this->db->order_by("name", "asc");
         $this->db->where('country', $data["code_country"]);
         $this->db->where('region', $data["code_region"]);
         $query = $this->db->get('GEO_Cities');
