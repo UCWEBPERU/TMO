e7fa46a7886b4fcfc684ed1e4b3edbb7520678a6
Mick Heral V.E <mickhve@gmail.com>
2016-02-22 17:43:39 -0500

    updates

--- resources/admin/js/ValidateInputFormWithParsley.js
+++ resources/admin/js/ValidateInputFormWithParsley.js
@@ -4,6 +4,8 @@ var ValidateInputFormWithParsley = {
         for (var i = 0; i < selectorInputsForm.length; i++) {
             if ($(selectorInputsForm[i]).parsley().isValid()) {
                 if ($(selectorInputsForm[i]).prop('tagName') == "SELECT") {
+                    console.log("SELECT2");
+                    console.log($(selectorInputsForm[i]).parent().find(".select2"));
                     $(selectorInputsForm[i]).parent().removeClass("has-error");
                     $(selectorInputsForm[i]).parent().find(".select2").find(".selection").children().removeClass("border-input-error");
                 } else {
@@ -12,6 +14,8 @@ var ValidateInputFormWithParsley = {
             } else {
                 if ($(selectorInputsForm[i]).prop('tagName') == "SELECT") {
                     $(selectorInputsForm[i]).parent().addClass("has-error");
+                    console.log("SELECT2");
+                    console.log($(selectorInputsForm[i]).parent().find(".select2"));
                     $(selectorInputsForm[i]).parent().find(".select2").find(".selection").children().addClass("border-input-error");
                 } else {
                     $(selectorInputsForm[i]).parent().addClass("has-error");
