3a0cca8b6243175379810fed79dd7c9e2e8d2a52
Mick Heral V.E <mickhve@gmail.com>
2016-02-22 17:25:49 -0500

    updates

--- resources/admin/js/ValidateInputFormWithParsley.js
+++ resources/admin/js/ValidateInputFormWithParsley.js
@@ -3,9 +3,17 @@ var ValidateInputFormWithParsley = {
         var messagesError = "";
         for (var i = 0; i < selectorInputsForm.length; i++) {
             if ($(selectorInputsForm[i]).parsley().isValid()) {
-                $(selectorInputsForm[i]).parent().removeClass("has-error");
+                if ($(selectorInputsForm[i]).parent().is("div")) {
+                    $(selectorInputsForm[i]).parent().removeClass("has-error");
+                } else {
+                    $(selectorInputsForm[i]).parent().removeClass("border-input-error");
+                }
             } else {
-                $(selectorInputsForm[i]).parent().addClass("has-error");
+                if ($(selectorInputsForm[i]).parent().is("div")) {
+                    $(selectorInputsForm[i]).parent().addClass("has-error");
+                } else {
+                    $(selectorInputsForm[i]).parent().addClass("border-input-error");
+                }
                 messagesError += "<li>" + ParsleyUI.getErrorsMessages($(selectorInputsForm[i]).parsley()) + "</li>";
             }
         }
