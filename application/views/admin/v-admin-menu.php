ab452a25069cbcc023493c50e1af9715b11e620f
Mick Heral V.E <mickhve@gmail.com>
2016-02-22 13:05:46 -0500

    updates

--- application/views/admin/v-admin-menu.php
+++ application/views/admin/v-admin-menu.php
@@ -7,7 +7,7 @@
 			<li class="header">MENU</li>
             <li <?php echo ($menu == 1) ? 'class="active"' : ''; ?>><a href="admin/empresa"><i class="fa fa-building-o"></i> <span>Company</span></a></li>
             <li <?php echo ($menu == 2) ? 'class="active"' : ''; ?>><a href="admin/tipoempresa"><i class="fa fa-building-o"></i> <span>Business type</span></a></li>
-            <li <?php echo ($menu == 3) ? 'class="active"' : ''; ?>><a href="admin/paquetes-tmo"><i class="fa fa-building-o"></i> <span>Paquetes TMO</span></a></li>
+            <li <?php echo ($menu == 3) ? 'class="active"' : ''; ?>><a href="admin/paquetes-tmo"><i class="fa fa-list-alt"></i> <span>Paquetes TMO</span></a></li>
             <li <?php echo ($menu == 4) ? 'class="active"' : ''; ?>><a href="admin/usuario-store"><i class="fa fa-users"></i> <span>User</span></a></li>
             <li <?php echo ($menu == 5) ? 'class="active"' : ''; ?>><a href="admin/perfil"><i class="fa fa-user"></i> <span>Profile</span></a></li>
 		</ul><!-- /.sidebar-menu -->
