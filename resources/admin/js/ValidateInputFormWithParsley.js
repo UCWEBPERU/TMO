ba56a24a049392811c4cb5a3c94cfccf45d78ab4
Mick Heral V.E <mickhve@gmail.com>
2016-02-22 17:42:01 -0500

    updates

--- resources/admin/js/ValidateInputFormWithParsley.js
+++ resources/admin/js/ValidateInputFormWithParsley.js
@@ -3,18 +3,18 @@ var ValidateInputFormWithParsley = {
         var messagesError = "";
         for (var i = 0; i < selectorInputsForm.length; i++) {
             if ($(selectorInputsForm[i]).parsley().isValid()) {
-                //alert("2 " + $(selectorInputsForm[i]).parent().prop('tagName'));
-                if ($(selectorInputsForm[i]).parent().prop('tagName') == "DIV") {
+                if ($(selectorInputsForm[i]).prop('tagName') == "SELECT") {
                     $(selectorInputsForm[i]).parent().removeClass("has-error");
+                    $(selectorInputsForm[i]).parent().find(".select2").find(".selection").children().removeClass("border-input-error");
                 } else {
-                    $(selectorInputsForm[i]).parent().removeClass("border-input-error");
+                    $(selectorInputsForm[i]).parent().removeClass("has-error");
                 }
             } else {
-                //alert("1 " + $(selectorInputsForm[i]).parent().is("div"));
-                if ($(selectorInputsForm[i]).parent().prop('tagName') == "DIV") {
+                if ($(selectorInputsForm[i]).prop('tagName') == "SELECT") {
                     $(selectorInputsForm[i]).parent().addClass("has-error");
+                    $(selectorInputsForm[i]).parent().find(".select2").find(".selection").children().addClass("border-input-error");
                 } else {
-                    $(selectorInputsForm[i]).parent().addClass("border-input-error");
+                    $(selectorInputsForm[i]).parent().addClass("has-error");
                 }
                 messagesError += "<li>" + ParsleyUI.getErrorsMessages($(selectorInputsForm[i]).parsley()) + "</li>";
             }
