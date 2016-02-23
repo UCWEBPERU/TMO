2b046de33ece59d74bdc5a18fa69e35d1fee4693
Mick Heral V.E <mickhve@gmail.com>
2016-02-23 16:57:34 -0500

    updates

--- application/views/admin/module/usuario/v-admin-usuario-rows.php
+++ application/views/admin/module/usuario/v-admin-usuario-rows.php
@@ -6,8 +6,8 @@
 	<td><?php echo $modeloRow->nombres_persona." ".$modeloRow->apellidos_persona; ?></td>
 	<td><?php echo $modeloRow->email_usuario; ?></td>
 	<td><?php echo $modeloRow->organization; ?></td>
-	<td><a href="<?php echo base_url()."store/".intval($modeloRow->id_empresa)."/admin"; ?>" target="_blank" ><?php echo base_url()."store/".intval($modeloRow->id_empresa)."/admin"; ?> <i class="fa fa-external-link"></i></a></td>
-	<td><a href="<?php echo base_url()."store/".intval($modeloRow->id_empresa); ?>" target="_blank" ><?php echo base_url()."store/".intval($modeloRow->id_empresa); ?> <i class="fa fa-external-link"></i></a></td>
+	<td><a href="<?php echo base_url()."company/".intval($modeloRow->id_empresa)."/admin"; ?>" target="_blank" ><?php echo base_url()."company/".intval($modeloRow->id_empresa)."/admin"; ?> <i class="fa fa-external-link"></i></a></td>
+	<td><a href="<?php echo base_url()."company/".intval($modeloRow->id_empresa); ?>" target="_blank" ><?php echo base_url()."company/".intval($modeloRow->id_empresa); ?> <i class="fa fa-external-link"></i></a></td>
 	<td><?php $fecha_registro = new DateTime($modeloRow->fecha_registro_usuario); echo date_format($fecha_registro, "Y-m-d"); ?></td>
 	<!--<td>
 		<a href="<?php echo base_url().$modulo->base_url.intval($modeloRow->id_empresa); ?>" data-row-type="empresa" data-row-action="edit" data-row-id="<?php echo $modeloRow->id_empresa; ?>" class="btnActionRow"><span class="label label-primary">Editar</span>
