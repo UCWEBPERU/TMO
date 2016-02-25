47fa8b70efee08b74c12229e3fe1f1ccb44c01d9
Mick Heral V.E <mickhve@gmail.com>
2016-02-25 16:50:38 -0500

    updates

--- application/models/M_Empresa.php
+++ application/models/M_Empresa.php
@@ -19,17 +19,17 @@ class M_Empresa extends CI_Model{
 	}
 
 	public function getEmpresaByName($nombre_empresa) {
-		$this->db->select("Empresa.id_empresa,
-							Empresa.nombres_representante,
-							Empresa.apellidos_representante,
-							Empresa.email_representante,
-							Empresa.celular_personal,
-							Empresa.telefono,
-							Empresa.celular_trabajo,
-							Empresa.fax,
-							Empresa.direccion,
-							Empresa.direccion_2,
-							Tipo_Empresa.nombre_tipo_empresa");
+//		$this->db->select("Empresa.id_empresa,
+//							Empresa.nombres_representante,
+//							Empresa.apellidos_representante,
+//							Empresa.email_representante,
+//							Empresa.celular_personal,
+//							Empresa.telefono,
+//							Empresa.celular_trabajo,
+//							Empresa.fax,
+//							Empresa.direccion,
+//							Empresa.direccion_2,
+//							Tipo_Empresa.nombre_tipo_empresa");
 		$this->db->join('Tipo_Empresa', 'Tipo_Empresa.id_tipo_empresa = Empresa.id_tipo_empresa');
 		$this->db->where('Empresa.estado', '1');
 		$this->db->where('Empresa.organization', $nombre_empresa);
