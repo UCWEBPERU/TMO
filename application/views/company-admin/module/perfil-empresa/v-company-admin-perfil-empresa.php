03274e49d48a639ed9a287f3f3d5188010163a06
Mick Heral V.E <mickhve@gmail.com>
2016-02-25 10:30:22 -0500

    updates

--- application/views/company-admin/module/perfil-empresa/v-company-admin-perfil-empresa.php
+++ application/views/company-admin/module/perfil-empresa/v-company-admin-perfil-empresa.php
@@ -250,7 +250,7 @@
                                         <div class="box-logo-store">
                                             <div class="logo-store-inner" style="background-image:url(&quot;<?php echo $modulo->icono_empresa; ?>&quot;);" data-reactid=".0.5.$=1$UserSettingsModal.0.0.1.0.$ACCOUNT.0.1.1.0">
                                                 <div class="logo-store-hint" data-reactid=".0.5.$=1$UserSettingsModal.0.0.1.0.$ACCOUNT.0.1.1.0.1">Cambiar Avatar</div>
-                                                <input style="top:0;left:0;width:100%;height:100%;opacity:0;cursor:pointer;" class="file-input" type="file" data-reactid=".0.5.$=1$UserSettingsModal.0.0.1.0.$ACCOUNT.0.1.1.0.2">
+                                                <input style="top:0;left:0;width:100%;height:100%;opacity:0;cursor:pointer;" type="file" id="imgLogoStore" name="imgLogoStore">
                                             </div>
 <!--                                            <img id="logoStore" class="img-size-50" src="--><?php //echo $modulo->icono_empresa; ?><!--" alt="Logo Store" title="Logo Store">-->
 <!--                                            <div class="btn btn-default btn-file">-->
@@ -515,7 +515,8 @@
                 formData.append("imgLogoStore", file);
             },
             function(readResult) {
-                $("#logoStore").attr("src", readResult);
+                $(".logo-store-inner").attr("style", "background-image: url('" + readResult + "');");
+//                $("#logoStore").attr("src", readResult);
             }
         );
 
