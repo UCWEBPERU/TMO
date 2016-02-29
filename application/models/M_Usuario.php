9ab8861f2e0e7768579cb219b27ac9169acf95af
Mick Heral V.E <mickhve@gmail.com>
2016-02-29 17:04:46 -0500

    updates

--- application/models/M_Usuario.php
+++ application/models/M_Usuario.php
@@ -44,10 +44,10 @@ class M_Usuario extends CI_Model {
             Persona.telefono,
             Persona.celular_trabajo,
             Persona.direccion_persona");
-		$this->db->join('Persona', 'Persona.id_usuario = Usuario.id_usuario');
-		$this->db->join('Tipo_Usuario', 'Tipo_Usuario.id_tipo_usuario = Usuario.id_tipo_usuario');
 		$this->db->join('Usuarios_Asignados', 'Usuarios_Asignados.id_usuario = Usuario.id_usuario');
 		$this->db->join('Empresa', 'Empresa.id_empresa = Usuarios_Asignados.id_empresa');
+		$this->db->join('Persona', 'Persona.id_usuario = Usuario.id_usuario');
+		$this->db->join('Tipo_Usuario', 'Tipo_Usuario.id_tipo_usuario = Usuario.id_tipo_usuario');
 		$this->db->where('Usuario.id_usuario', $data["id_usuario"]);
 		$this->db->where('Empresa.id_empresa', $data["id_empresa"]);
 		$this->db->where('Empresa.estado', 1);
