1e0c7d10f451957436e6e56fc810ea9256499e85
Mick Heral V.E <mickhve@gmail.com>
2016-03-02 15:46:04 -0500

    updates

--- application/views/company-admin/module/product/v-company-admin-product-agregar.php
+++ application/views/company-admin/module/product/v-company-admin-product-agregar.php
@@ -57,7 +57,6 @@
                                 <div class="form-group">
                                     <label>Categoria</label>
                                     <select class="form-control select2" style="width: 100%;" id="cboCategorias" name="cboCategorias" multiple="multiple">
-                                        <option selected="selected" value="">Tienda</option>
                                         <?php foreach($modulo->data_tiendas as $tienda): ?>
                                             <option value="<?php echo $tienda->id_tienda; ?>"><?php echo $tienda->nombre_tienda; ?></option>
                                         <?php endforeach; ?>
