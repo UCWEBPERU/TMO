4f3258823d6b0f70820cb5ac3a7c653eff4a54d4
Mick Heral V.E <mickhve@gmail.com>
2016-03-04 14:38:47 -0500

    updates

--- application/views/company-admin/module/product/v-company-admin-product-agregar.php
+++ application/views/company-admin/module/product/v-company-admin-product-agregar.php
@@ -308,7 +308,7 @@
 //                                        return false;
 //                                    }
                                     swal("Modificador agregado!", tipoModificador + ": " + nombreModificador, "success");
-                                    var html = "<tr><td>" + $(".table-modifiers tbody").length - 2 + 1 + ".</td><td>" + tipoModificador + "</td><td>" + nombreModificador + "</td><td>" + costoModificador + "</td></tr>";
+                                    var html = "<tr><td>" + (($(".table-modifiers tbody tr").length - 2) + 1) + ".</td><td>" + tipoModificador + "</td><td>" + nombreModificador + "</td><td>" + costoModificador + "</td></tr>";
                                     $(".table-modifiers tbody tr").last().before(html);
                                     console.log($(".table-modifiers tbody tr").last());
                                 }
