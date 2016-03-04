726dbcc78843a046bc4bbbbdc6575c5771efcb2d
Mick Heral V.E <mickhve@gmail.com>
2016-03-04 12:28:02 -0500

    updates

--- application/views/company-admin/module/product/v-company-admin-product-agregar.php
+++ application/views/company-admin/module/product/v-company-admin-product-agregar.php
@@ -29,7 +29,7 @@
         <section class="content">
 
             <div class="row">
-                <div class="col-md-12">
+                <div class="col-md-6">
                     <div class="box box-primary">
                         <div class="box-header with-border">
                             <h3 class="box-title">Datos Producto</h3>
