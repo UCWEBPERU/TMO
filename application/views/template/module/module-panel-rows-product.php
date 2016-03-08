160b337bb284439060f71fb852fde69eb642a81f
mickheral <mickhve@gmail.com>
2016-03-07 21:40:27 -0500

    updates

--- application/views/template/module/module-panel-rows-product.php
+++ application/views/template/module/module-panel-rows-product.php
@@ -7,7 +7,7 @@
 			<td><?php echo $modeloRow->nombre_producto; ?></td>
 			<td><?php echo $modeloRow->descripcion_producto; ?></td>
 			<td><?php echo $modeloRow->stock; ?></td>
-			<td>$<?php echo number_format($modeloRow->precio_producto, 2, "."); ?></td>
+			<td>$<?php echo number_format($modeloRow->precio_producto, 2, ".", ""); ?></td>
 			<td><?php echo $modeloRow->nombre_tienda; ?></td>
 			<td>
 				<a href="<?php echo $modulo->url_module_panel."/edit/".intval($modeloRow->id_producto) ?>" data-row-type="user" data-row-action="edit" data-row-id="<?php echo $modeloRow->id_producto; ?>" class="btnActionRow"><span class="label label-primary">Editar</span></a>&nbsp;&nbsp;
