5840392f5d85857b120675b1e2f4ec707d6a7816
Mick Heral V.E <mickhve@gmail.com>
2016-03-11 11:59:05 -0500

    updates

--- application/config/routes.php
+++ application/config/routes.php
@@ -207,12 +207,17 @@ $route['company/(:num)/admin/category/ajax/updateImageCategory']    = 'company-a
 
 /*
 | ------------------------------
-| URL -> COMPANY ADMIN - PRODUCTOS CATEGORY
+| URL -> STORE - HOME
 | ------------------------------
 */
-$route['company/(:num)/store/(:num)'] = 'store/C_Store_Home';
-$route['company/(:num)/store/(:num)/categories/(:num)'] = 'store/C_Store_Home';
-$route['company/(:num)/store/(:num)/categories/(:num).(:num)'] = 'store/C_Store_Home';
+$route['company/(:num)/store/(:num)']                                               = 'store/C_Store_Home';
+
+// url para soportar el anidamiento de las subcategorias
+$route['company/(:num)/store/(:num)/categories/(:num)']                             = 'store/C_Store_Home';
+$route['company/(:num)/store/(:num)/categories/(:num).(:num)']                      = 'store/C_Store_Home';
+$route['company/(:num)/store/(:num)/categories/(:num).(:num).(:num)']               = 'store/C_Store_Home';
+$route['company/(:num)/store/(:num)/categories/(:num).(:num).(:num).(:num)']        = 'store/C_Store_Home';
+$route['company/(:num)/store/(:num)/categories/(:num).(:num).(:num).(:num).(:num)'] = 'store/C_Store_Home';
 
 ///*
 //| ------------------------------
--- application/controllers/store/C_Store_Home.php
+++ application/controllers/store/C_Store_Home.php
@@ -11,7 +11,6 @@ class C_Store_Home extends CI_Controller {
     }
 
     public function index() {
-
         $modulo = new stdClass();
 
         $dataEmpresa = $this->M_Store_Home->getCompanyAndStore(
@@ -25,48 +24,92 @@ class C_Store_Home extends CI_Controller {
             redirect("not-found/store");
         }
 
-        $dataCategorias = $this->M_Store_Home->getPrimaryCategories($this->uri->segment(2));
+        $dataCategorias = cargarDatosCategoriasPrincipales();
         $modulo->data_categorias = $dataCategorias;
 
         if (sizeof($dataCategorias) > 0) {
-            $dataSubCategorias = $this->M_Store_Home->getSubCategories(
-                array(
-                    "id_empresa"            => $this->uri->segment(2),
-                    "id_categoria_superior" => $dataCategorias[0]->id_categoria
-                )
-            );
-            $modulo->data_sub_categorias = $dataSubCategorias;
-
-            $dataProductos = $this->M_Store_Home->getProducts(
-                array(
-                    "id_empresa"    => $this->uri->segment(2),
-                    "id_tienda"     => $this->uri->segment(4),
-                    "id_categoria"  => $dataCategorias[0]->id_categoria
-                )
-            );
-
-            foreach ($dataProductos as $producto) {
-                $geleriaProducto = $this->M_Store_Home->getGalleryByProduct(
-                    array(
-                        "id_producto"    => $producto->id_producto
-                    )
-                );
-
-                if (sizeof($geleriaProducto) > 0) {
-                    $producto->galeria_producto = $geleriaProducto;
-                } else {
-                    $producto->galeria_producto = array(
-                        "url_archivo" => base_url().PATH_RESOURCE_ADMIN."img/image_not_found.png"
-                    );
-                }
+            $modulo->data_sub_categorias = cargarDatosSubCategorias($dataCategorias);
+            $modulo->data_productos = cargarDatosProductos($dataCategorias);
+            foreach ($modulo->data_productos as $producto) {
+                $producto = cargarGaleriaPorProducto($producto);
             }
-
-            $modulo->data_productos = $dataProductos;
         }
 
         $data["modulo"] = $modulo;
 
         $this->load->view('store/v-store-home', $data);
+    }
+
+    public function viewSubCategorias() {
+        $modulo = new stdClass();
+
+        $dataEmpresa = $this->M_Store_Home->getCompanyAndStore(
+            array(
+                "id_empresa"    => $this->uri->segment(2),
+                "id_tienda"     => $this->uri->segment(4)
+            )
+        );
+
+        if (sizeof($dataEmpresa) == 0) {
+            redirect("not-found/store");
+        }
+
+        $dataCategorias = cargarDatosCategoriasPrincipales();
+        $modulo->data_categorias = $dataCategorias;
+
+        if (sizeof($dataCategorias) > 0) {
+            $modulo->data_sub_categorias = cargarDatosSubCategorias($dataCategorias);
+            $modulo->data_productos = cargarDatosProductos($dataCategorias);
+            foreach ($modulo->data_productos as $producto) {
+                $producto = cargarGaleriaPorProducto($producto);
+            }
+        }
+
+        $data["modulo"] = $modulo;
+
+        $this->load->view('store/v-store-home', $data);
+    }
+
+    public function cargarDatosCategoriasPrincipales() {
+        return $this->M_Store_Home->getPrimaryCategories($this->uri->segment(2));
+    }
+
+    public function cargarDatosSubCategorias($dataCategorias) {
+        $dataSubCategorias = $this->M_Store_Home->getSubCategories(
+            array(
+                "id_empresa"            => $this->uri->segment(2),
+                "id_categoria_superior" => $dataCategorias[0]->id_categoria
+            )
+        );
+        return $dataSubCategorias;
+    }
+
+    public function cargarDatosProductos($dataCategorias) {
+        $dataProductos = $this->M_Store_Home->getProducts(
+            array(
+                "id_empresa"    => $this->uri->segment(2),
+                "id_tienda"     => $this->uri->segment(4),
+                "id_categoria"  => $dataCategorias[0]->id_categoria
+            )
+        );
+        return $dataProductos;
+    }
+
+    public function cargarGaleriaPorProducto($producto) {
+        $geleriaProducto = $this->M_Store_Home->getGalleryByProduct(
+            array(
+                "id_producto"    => $producto->id_producto
+            )
+        );
+
+        if (sizeof($geleriaProducto) > 0) {
+            $producto->galeria_producto = $geleriaProducto;
+        } else {
+            $producto->galeria_producto = array(
+                "url_archivo" => base_url().PATH_RESOURCE_ADMIN."img/image_not_found.png"
+            );
+        }
+        return $producto;
     }
 
 }
