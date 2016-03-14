a77776cd13fd6451b1c758ef0685f33011aa2f69
Mick Heral V.E <mickhve@gmail.com>
2016-03-14 10:17:26 -0500

    updates

--- application/models/store/M_Store_Product.php
+++ application/models/store/M_Store_Product.php
@@ -72,8 +72,8 @@ class M_Store_Product extends CI_Model {
         $this->db->join('Tienda', 'Tienda.id_tienda = Catalogo_Productos.id_tienda');
         $this->db->join('Categoria_Productos', 'Categoria_Productos.id_categoria = Producto.id_categoria');
         $this->db->where('Categoria_Productos.id_empresa', $data["id_empresa"]);
-        $this->db->where('Categoria_Productos.id_categoria', $data["id_categoria"]);
         $this->db->where('Producto.id_producto', $data["id_producto"]);
+        $this->db->where('Tienda.id_tienda', $data["id_tienda"]);
         $this->db->where('Categoria_Productos.estado', '1');
         $this->db->where('Tienda.estado', '1');
         $this->db->where('Producto.estado', '1');
