86b4e1b060922e531d5d77c9540b29a920533ff3
Mick Heral V.E <mickhve@gmail.com>
2016-03-15 18:04:23 -0500

    updates

--- application/views/store/v-store-search.php
+++ application/views/store/v-store-search.php
@@ -72,7 +72,7 @@
     <div class="col-xs-12" >
         <div class="row">
             <div class="col-xs-12" id="find">
-                <input type="text" placeholder="Search <?php echo strtoupper($modulo->data_empresa); ?>">
+                <input type="text" placeholder="Search <?php echo strtoupper($modulo->data_empresa->organization); ?>">
             </div>
         </div>
         <div class="row"  >
