5febff67f132f3a3e930caf6284a050d438d5fe3
Mick Heral V.E <mickhve@gmail.com>
2016-03-15 17:53:27 -0500

    updates

--- application/controllers/store/C_Store_Search.php
+++ application/controllers/store/C_Store_Search.php
@@ -13,6 +13,26 @@ class C_Store_Search extends CI_Controller {
         $modulo = new stdClass();
         $modulo->base_url_store = base_url()."company/".$this->uri->segment(2)."/store/".$this->uri->segment(4);
 
+        $dataEmpresa = $this->M_Store_Home->getCompanyAndStore(
+            array(
+                "id_empresa"    => $this->uri->segment(2),
+                "id_tienda"     => $this->uri->segment(4)
+            )
+        );
+
+        if (sizeof($dataEmpresa) == 0) {
+            redirect("not-found/store");
+        }
+
+        $dataCategorias = $this->cargarDatosCategoriasPrincipales();
+        $modulo->data_categorias = $dataCategorias;
+        $modulo->id_categoria_raiz = $dataCategorias[0]->id_categoria;
+
+
+        if (sizeof($dataCategorias) > 0) {
+
+        }
+
         $data["modulo"] = $modulo;
 
         $this->load->view('store/v-store-search', $data);
--- application/views/store/v-store-search.php
+++ application/views/store/v-store-search.php
@@ -51,28 +51,25 @@
 </script>
 
 <header>
-    <div id="title">
-        <h2>Fashion Store</h2>
-    </div>
     <!-- Nav lbslider -->
     <div id="jssor_1" style="position: relative; left: 0px; width: 980px; height: 100px; ">
         <!-- Loading Screen -->
         <div data-u="slides" id="slide" >
-            <div>
-                <a href="#" ><p>WOMEN</p></a>
-            </div>
-            <div>
-                <a href="#" ><p>MEN</p></a>
-            </div>
-            <div>
-                <a href="#" ><p>KIDS</p></a>
-            </div>
-            <div>
-                <a href="#" ><p>DESING</p></a>
-            </div>
-            <div>
-                <a href="#" ><p>FEATURED</p></a>
-            </div>
+            <?php for ( $c = 0; $c < sizeof($modulo->data_categorias); $c++) { ?>
+                <?php if ($modulo->data_categorias[$c]->id_categoria == $modulo->id_categoria_raiz) { ?>
+                    <div style="background:#FFF;">
+                        <a href="<?php echo $modulo->base_url_store."/categories/".intval($modulo->data_categorias[$c]->id_categoria); ?>" style="color:#000;">
+                            <p><?php echo strtoupper($modulo->data_categorias[$c]->nombre_categoria); ?></p>
+                        </a>
+                    </div>
+                <?php } else { ?>
+                    <div>
+                        <a href="<?php echo $modulo->base_url_store."/categories/".intval($modulo->data_categorias[$c]->id_categoria); ?>">
+                            <p><?php echo strtoupper($modulo->data_categorias[$c]->nombre_categoria); ?></p>
+                        </a>
+                    </div>
+                <?php } ?>
+            <?php } ?>
         </div>
     </div>
 </header>
