fba248e6e2f88bb4cf4a0b8ffd63de0fc806b49e
Mick Heral V.E <mickhve@gmail.com>
2016-03-15 16:27:35 -0500

    updates

--- application/views/store/v-store-product-detail.php
+++ application/views/store/v-store-product-detail.php
@@ -63,12 +63,14 @@
                         <?php } ?>
                     </div>
                     <div class="col-xs-12 detail" id="color">
-                        <h2>Color</h2>
-                        <?php foreach ($modulo->data_modifiers as $modifier) { ?>
+                        <?php
+                            $contador = 1;
+                            foreach ($modulo->data_modifiers as $modifier) { ?>
                             <?php if (trim(strtolower($modifier->tipo_modificador)) == "color") { ?>
-                                <?php if (isset($modifier->color_rgb)) { ?>
-                                    <button style="background: <?php echo $modifier->color_rgb; ?>;"></button>
-                                <?php } ?>
+                                <?php if (isset($modifier->color_rgb)) {
+                                    if ($contador == 1) { $contador++; ?>
+                                        <button style="background: <?php echo $modifier->color_rgb; ?>;"></button>
+                                <?php } } ?>
                             <?php } ?>
                         <?php } ?>
                     </div>
