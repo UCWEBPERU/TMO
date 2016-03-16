9232b153a64947efead23f275f54b968acae4453
Mick Heral V.E <mickhve@gmail.com>
2016-03-16 12:59:05 -0500

    updates

--- application/views/store/v-store-search-result.php
+++ application/views/store/v-store-search-result.php
@@ -65,19 +65,11 @@
         <!-- Loading Screen -->
         <div data-u="slides" id="slide" >
             <?php for ( $c = 0; $c < sizeof($modulo->data_categorias); $c++) { ?>
-                <?php if ($modulo->data_categorias[$c]->id_categoria == $modulo->id_categoria_raiz) { ?>
-                    <div style="background:#FFF;">
-                        <a href="<?php echo $modulo->base_url_store."/categories/".intval($modulo->data_categorias[$c]->id_categoria); ?>" style="color:#000;">
-                            <p><?php echo strtoupper($modulo->data_categorias[$c]->nombre_categoria); ?></p>
-                        </a>
-                    </div>
-                <?php } else { ?>
-                    <div>
-                        <a href="<?php echo $modulo->base_url_store."/categories/".intval($modulo->data_categorias[$c]->id_categoria); ?>">
-                            <p><?php echo strtoupper($modulo->data_categorias[$c]->nombre_categoria); ?></p>
-                        </a>
-                    </div>
-                <?php } ?>
+                <div>
+                    <a href="<?php echo $modulo->base_url_store."/categories/".intval($modulo->data_categorias[$c]->id_categoria); ?>">
+                        <p><?php echo strtoupper($modulo->data_categorias[$c]->nombre_categoria); ?></p>
+                    </a>
+                </div>
             <?php } ?>
         </div>
     </div>
