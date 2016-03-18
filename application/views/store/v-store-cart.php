3ba6715c41a09bdbb6851ccac66444b43c873c47
Sheyla <ucweb03@hotmail.com>
2016-03-18 15:46:32 -0500

    updates

--- application/views/store/v-store-cart.php
+++ application/views/store/v-store-cart.php
@@ -70,7 +70,7 @@
                             <div>
 
                                 <div class="col-xs-5 list"  id="cartitem" >
-                                    <a ><img src="<?php echo $item['url_image']; ?>" id="images" alt=""  /></a>
+                                    <a ><img src="<?php echo product_options($item['id'])['url_image']; ?>" id="images" alt=""  /></a>
                                     <a class="btn">Edit</a>
                                 </div>
                                 <div class="col-xs-7 list" id="cartitem" >
