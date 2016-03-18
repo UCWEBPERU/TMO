46d1a791a93a5fb79daaa978b2e913ae5e8d77a5
Sheyla <ucweb03@hotmail.com>
2016-03-18 16:32:39 -0500

    updates

--- application/views/store/v-store-cart.php
+++ application/views/store/v-store-cart.php
@@ -69,7 +69,7 @@
 
                             <div>
 
-                                <div class="col-xs-5 list"  id="cartitem" >
+                                <div class="col-xs-5 list"  id="cartitem" > 
                                     <a ><img src="<?php $options = $this->cart->product_options($item['rowid']); echo $options['url_image'] ?>" id="images" alt=""  /></a>
                                     <a class="btn">Edit</a>
                                 </div>
