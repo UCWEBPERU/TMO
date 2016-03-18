f69d6e618b3db05c7c8f67a68cb24535fcea27d9
Sheyla <ucweb03@hotmail.com>
2016-03-18 11:38:14 -0500

    updates

--- application/controllers/store/C_Store_Cart.php
+++ application/controllers/store/C_Store_Cart.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class C_Store_Account extends CI_Controller {
+class C_Store_Cart extends CI_Controller {
     
     public function __construct() {
         parent::__construct();
