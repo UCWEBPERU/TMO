bb6ea73f065a1c3d5e91d7bf87d59f8b08d646c2
Sheyla <ucweb03@hotmail.com>
2016-03-18 12:20:46 -0500

    updates

--- application/controllers/store/C_Store_Cart.php
+++ application/controllers/store/C_Store_Cart.php
@@ -15,6 +15,19 @@ class C_Store_Cart extends CI_Controller {
     function index(){
         $modulo = new stdClass();
         $modulo->base_url_store = base_url()."company/".$this->uri->segment(2)."/store/".$this->uri->segment(4);
+        $modulo->has_user_session = $this->usersession->isClient();
+
+        $dataEmpresa = $this->M_Store->getCompanyAndStore(
+            array(
+                "id_empresa"    => $this->uri->segment(2),
+                "id_tienda"     => $this->uri->segment(4)
+            )
+        );
+
+        if (sizeof($dataEmpresa) == 0) {
+            redirect("not-found/store");
+        }
+
         $data["modulo"] = $modulo;
         $this->load->view('store/v-store-cart',  $data);
 
