e60ca17bf16a2045dc7768e808851c37d92283a8
Sheylavc <sheylavc@hotmail.com>
2016-03-22 12:36:17 -0500

    updates

--- application/config/routes.php
+++ application/config/routes.php
@@ -225,6 +225,13 @@ $route['company/(:num)/store/(:num)/ajax/registerClient']       = 'store/C_Store
 $route['company/(:num)/store/(:num)/ajax/signIn']               = 'store/C_Store_Sign_In/ajaxSignIn';
 $route['company/(:num)/store/(:num)/ajax/forgotPassword']       = 'store/C_Store_Sign_In/ajaxForgotPassword';
 
+
+$route['company/(:num)/store/(:num)/products/(:num)/tags/(:num)']                   = 'store/C_Store_Product/viewProduct/$3.$4';
+$route['company/(:num)/store/(:num)/products/(:num)/tags/(:num).(:num)']            = 'store/C_Store_Product/viewProduct/$3.$4.$5';
+$route['company/(:num)/store/(:num)/products/(:num)/tags/(:num).(:num).(:num)']     = 'store/C_Store_Product/viewProduct/$3.$4.$5.$6';
+$route['company/(:num)/store/(:num)/products/(:num)/tags/(:num).(:num).(:num).(:num)']     = 'store/C_Store_Product/viewProduct/$3.$4.$5.$6.$7';
+$route['company/(:num)/store/(:num)/products/(:num)/tags/(:num).(:num).(:num).(:num).(:num)']     = 'store/C_Store_Product/viewProduct/$3.$4.$5.$6.$7.$8';
+
 // url para soportar el anidamiento de las subcategorias
 $route['company/(:num)/store/(:num)/categories/(:num)']                             = 'store/C_Store_Home/viewSubCategorias/$3';
 $route['company/(:num)/store/(:num)/categories/(:num).(:num)']                      = 'store/C_Store_Home/viewSubCategorias/$3.$4';
@@ -238,10 +245,10 @@ $route['company/(:num)/store/(:num)/categories/(:num).(:num).(:num).(:num).(:num
 | ------------------------------
 */
 
-$route['company/(:num)/store/(:num)/cart']                     = 'store/C_Store_Cart';
-$route['shopping/add']                                         = 'store/C_Store_Cart/addCart';
-$route['shopping/update']                                     = 'store/C_Store_Cart/updateCart';
-$route['shopping/delete']                                     = 'store/C_Store_Cart/deleteitemCart';
+$route['company/(:num)/store/(:num)/cart']                          = 'store/C_Store_Cart';
+$route['company/(:num)/store/(:num)/ajax/shopping/add']             = 'store/C_Store_Cart/addCart';
+$route['company/(:num)/store/(:num)/ajax/shopping/update']          = 'store/C_Store_Cart/updateCart';
+$route['company/(:num)/store/(:num)/ajax/shopping/delete']          = 'store/C_Store_Cart/deleteitemCart';
 
 ///*
 //| ------------------------------
