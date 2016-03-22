704f090e09b7f281f0cbaabe38944692a121a753
Mick Heral V.E <mickhve@gmail.com>
2016-03-22 15:18:37 -0500

    updates

--- index.php
+++ index.php
@@ -292,5 +292,5 @@ switch (ENVIRONMENT)
  *
  * And away we go...
  */
-require_once __DIR__.'/vendor/autoload.php';
+require_once 'vendor/autoload.php';
 require_once BASEPATH.'core/CodeIgniter.php';
