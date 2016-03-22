1a55ab63da656633bc072ea0a8a1950ada9ec5b0
Mick Heral V.E <mickhve@gmail.com>
2016-03-22 16:08:59 -0500

    updates

--- application/controllers/store/C_Store_Checkout.php
+++ application/controllers/store/C_Store_Checkout.php
@@ -17,13 +17,12 @@ class C_Store_Checkout extends CI_Controller {
 
     public function index() {
 
-//        $gateway = Omnipay::create('PayPal_Express');
-//        $gateway->setUsername('adrian');
-//        $gateway->setPassword('12345');
-//        $settings = $gateway->getDefaultParameters();
+        $gateway = Omnipay::create('PayPal_Express');
+        $gateway->setUsername('adrian');
+        $gateway->setPassword('12345');
 
-        $gateway = Omnipay::create('Stripe');
-        $gateway->setApiKey('abc123');
+//        $gateway = Omnipay::create('Stripe');
+//        $gateway->setApiKey('abc123');
 
         $formData = array('number' => '4242424242424242', 'expiryMonth' => '6', 'expiryYear' => '2016', 'cvv' => '123');
         $response = $gateway->purchase(array('amount' => '10.00', 'currency' => 'USD', 'card' => $formData))->send();
