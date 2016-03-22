20aeba3303284342c564196fce54637cfdc7106f
Mick Heral V.E <mickhve@gmail.com>
2016-03-22 16:07:33 -0500

    updates

--- application/controllers/store/C_Store_Checkout.php
+++ application/controllers/store/C_Store_Checkout.php
@@ -17,10 +17,28 @@ class C_Store_Checkout extends CI_Controller {
 
     public function index() {
 
-        $gateway = Omnipay::create('PayPal_Express');
-        $gateway->setUsername('adrian');
-        $gateway->setPassword('12345');
-        $settings = $gateway->getDefaultParameters();
+//        $gateway = Omnipay::create('PayPal_Express');
+//        $gateway->setUsername('adrian');
+//        $gateway->setPassword('12345');
+//        $settings = $gateway->getDefaultParameters();
+
+        $gateway = Omnipay::create('Stripe');
+        $gateway->setApiKey('abc123');
+
+        $formData = array('number' => '4242424242424242', 'expiryMonth' => '6', 'expiryYear' => '2016', 'cvv' => '123');
+        $response = $gateway->purchase(array('amount' => '10.00', 'currency' => 'USD', 'card' => $formData))->send();
+
+        if ($response->isSuccessful()) {
+            // payment was successful: update database
+            print_r($response);
+        } elseif ($response->isRedirect()) {
+            // redirect to offsite payment gateway
+            $response->redirect();
+        } else {
+            // payment failed: display message to customer
+            echo $response->getMessage();
+        }
+
     }
 
 }
