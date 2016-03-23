8b6c9ff63d80b212f37e82fbeba060cdb07ddbc2
Mick Heral V.E <mickhve@gmail.com>
2016-03-23 11:48:11 -0500

    updates

--- application/controllers/store/C_Store_Checkout.php
+++ application/controllers/store/C_Store_Checkout.php
@@ -18,7 +18,7 @@ class C_Store_Checkout extends CI_Controller {
     public function index() {
 
         $gateway = Omnipay::create('Stripe');
-        $gateway->setApiKey('sk_test_CrIMteM3hZEr98Qj3KKOiYCm');
+        $gateway->setApiKey('sk_live_Y8KSanX283UzDT2OZ2xpNann');
 
         $formData = array('number' => '4242424242424242', 'expiryMonth' => '6', 'expiryYear' => '2016', 'cvv' => '123');
         $response = $gateway->purchase(array('amount' => '10.00', 'currency' => 'USD', 'card' => $formData))->send();
