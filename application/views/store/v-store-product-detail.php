168d42fcd250003a01ba3e92b0065322984dc37f
Sheylavc <sheylavc@hotmail.com>
2016-03-23 09:50:36 -0500

    updates

--- application/views/store/v-store-product-detail.php
+++ application/views/store/v-store-product-detail.php
@@ -152,6 +152,10 @@
                 <h2><a href="<?php echo $modulo->base_url_store; ?>" onclick="">Home</a></h2>
             </div>
             <div class="boximage">
+                <a href="<?php echo $modulo->base_url_store; ?>"><img src="<?php echo PATH_RESOURCE_STORE; ?>images/sale.png" class="images" alt="" /></a>
+                <h2><a href="<?php echo $modulo->base_url_store; ?>" onclick="">Home</a></h2>
+            </div>
+            <div class="boximage">
                 <a href="<?php echo $modulo->base_url_store; ?>/search"><img src="<?php echo PATH_RESOURCE_STORE; ?>images/tool.png" class="images" alt="" /></a>
                 <h1><a href="<?php echo $modulo->base_url_store; ?>/search" onclick="">Find</a></h1>
             </div>
