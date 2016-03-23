11e98b0072e0b93cef43dee7f136a43ce99b1d33
Mick Heral V.E <mickhve@gmail.com>
2016-03-23 11:01:32 -0500

    updates

--- application/controllers/store/C_Store_Checkout.php
+++ application/controllers/store/C_Store_Checkout.php
@@ -17,22 +17,23 @@ class C_Store_Checkout extends CI_Controller {
 
     public function index() {
 
-//        $gateway = Omnipay::create('Stripe');
-//        $gateway->setApiKey('sk_test_CrIMteM3hZEr98Qj3KKOiYCm');
-//
-//        $formData = array('number' => '4242424242424242', 'expiryMonth' => '6', 'expiryYear' => '2016', 'cvv' => '123');
-//        $response = $gateway->purchase(array('amount' => '10.00', 'currency' => 'USD', 'card' => $formData))->send();
-//
-//        if ($response->isSuccessful()) {
-//            // payment was successful: update database
-//            print_r($response);
-//        } elseif ($response->isRedirect()) {
-//            // redirect to offsite payment gateway
-//            $response->redirect();
-//        } else {
-//            // payment failed: display message to customer
-//            echo $response->getMessage();
-//        }
+        $gateway = Omnipay::create('Stripe');
+        $gateway->setApiKey('sk_test_CrIMteM3hZEr98Qj3KKOiYCm');
+
+        $formData = array('number' => '4242424242424242', 'expiryMonth' => '6', 'expiryYear' => '2016', 'cvv' => '123');
+        $response = $gateway->purchase(array('amount' => '10.00', 'currency' => 'USD', 'card' => $formData))->send();
+
+        if ($response->isSuccessful()) {
+            // payment was successful: update database
+            echo $auth_id = $response->getTransactionReference();
+            print_r($response);
+        } elseif ($response->isRedirect()) {
+            // redirect to offsite payment gateway
+            $response->redirect();
+        } else {
+            // payment failed: display message to customer
+            echo $response->getMessage();
+        }
 
 //        $gateway = Omnipay::create('PayPal_Express');
 //        $gateway->setUsername('sales@lab.design');
@@ -66,52 +67,52 @@ class C_Store_Checkout extends CI_Controller {
 //         (routes to GatewayFactory::create)
 //        phpinfo();
 
-        $gateway = Omnipay::create('PayPal_Rest');
-
-        // Initialise the gateway
+//        $gateway = Omnipay::create('PayPal_Rest');
+//
+//        // Initialise the gateway
+////        $gateway->initialize(array(
+////            'clientId' => 'AVg1I_sUtJ0o9EAjrOc0RcqTUPIZWScTjc_Goj7A0DSkbeIEts0oEf6w9zECrdSe-p8opzJtUCKdWab8',
+////            'secret'   => 'EDpduhPbvFGXfWV23YvnifwEGrnHg3hA-v-RvgL1K9uk_xkDaat4XYNOVguvkENCercJYC8Je-12CYXH',
+////            'testMode' => true, // Or false when you are ready for live transactions
+////        ));
 //        $gateway->initialize(array(
-//            'clientId' => 'AVg1I_sUtJ0o9EAjrOc0RcqTUPIZWScTjc_Goj7A0DSkbeIEts0oEf6w9zECrdSe-p8opzJtUCKdWab8',
-//            'secret'   => 'EDpduhPbvFGXfWV23YvnifwEGrnHg3hA-v-RvgL1K9uk_xkDaat4XYNOVguvkENCercJYC8Je-12CYXH',
+//            'clientId' => 'Ac-DjRm6aIjZdRWOUt4TbynQUlVfErqzMjh_7cxJB2Dc-dYqlDZoBSptF8yZ8eOTwscMcZD6eAYFwUj9',
+//            'secret'   => 'EJRC21ay3VTyuoMguOHEnt8d3vwLR4kuI7eejaYAUbYLAG5ogrcrQd1T1YcIV5IxuGHIrkOZ-rhLjng6',
 //            'testMode' => true, // Or false when you are ready for live transactions
 //        ));
-        $gateway->initialize(array(
-            'clientId' => 'Ac-DjRm6aIjZdRWOUt4TbynQUlVfErqzMjh_7cxJB2Dc-dYqlDZoBSptF8yZ8eOTwscMcZD6eAYFwUj9',
-            'secret'   => 'EJRC21ay3VTyuoMguOHEnt8d3vwLR4kuI7eejaYAUbYLAG5ogrcrQd1T1YcIV5IxuGHIrkOZ-rhLjng6',
-            'testMode' => true, // Or false when you are ready for live transactions
-        ));
-
-
-
-        // Create a credit card object
-        // DO NOT USE THESE CARD VALUES -- substitute your own
-        // see the documentation in the class header.
-        $card = array(
-            'firstName' => 'Example',
-            'lastName' => 'User',
-            'number' => '4111111111111111',
-            'expiryMonth'           => '01',
-            'expiryYear'            => '2020',
-            'cvv'                   => '123',
-            'billingAddress1'       => '1 Scrubby Creek Road',
-            'billingCountry'        => 'AU',
-            'billingCity'           => 'Scrubby Creek',
-            'billingPostcode'       => '4999',
-            'billingState'          => 'QLD',
-        );
-
-        // Do an authorisation transaction on the gateway
-        $transaction = $gateway->authorize(array(
-            'amount'        => '10.00',
-            'currency'      => 'AUD',
-            'description'   => 'This is a test authorize transaction.',
-            'card'          => $card,
-        ));
-        $response = $transaction->send();
-        if ($response->isSuccessful()) {
-            echo "Authorize transaction was successful!\n";
-            // Find the authorization ID
-            $auth_id = $response->getTransactionReference();
-        }
+//
+//
+//
+//        // Create a credit card object
+//        // DO NOT USE THESE CARD VALUES -- substitute your own
+//        // see the documentation in the class header.
+//        $card = array(
+//            'firstName' => 'Example',
+//            'lastName' => 'User',
+//            'number' => '4111111111111111',
+//            'expiryMonth'           => '01',
+//            'expiryYear'            => '2020',
+//            'cvv'                   => '123',
+//            'billingAddress1'       => '1 Scrubby Creek Road',
+//            'billingCountry'        => 'AU',
+//            'billingCity'           => 'Scrubby Creek',
+//            'billingPostcode'       => '4999',
+//            'billingState'          => 'QLD',
+//        );
+//
+//        // Do an authorisation transaction on the gateway
+//        $transaction = $gateway->authorize(array(
+//            'amount'        => '10.00',
+//            'currency'      => 'AUD',
+//            'description'   => 'This is a test authorize transaction.',
+//            'card'          => $card,
+//        ));
+//        $response = $transaction->send();
+//        if ($response->isSuccessful()) {
+//            echo "Authorize transaction was successful!\n";
+//            // Find the authorization ID
+//            $auth_id = $response->getTransactionReference();
+//        }
 
     }
 
