a0b4e1f298be509108c1ec4a63390fd5d394b56c
Sheylavc <sheylavc@hotmail.com>
2016-03-23 16:28:17 -0500

    updates

--- application/views/store/v-store-product-detail.php
+++ application/views/store/v-store-product-detail.php
@@ -237,7 +237,7 @@
                 formData.append("precio_producto", precio_producto);
                 for(var i = 0 ; i< listaModificadoresSeleccionados.length; i++){
                     var m = "modificador".concat(i);
-                    formData.append(m, listaModificadoresSeleccionados[i].id);
+                    formData.append("modificador", listaModificadoresSeleccionados[i].id);
 
                 }
                 //formData.append("modificadores_producto", listaModificadoresSeleccionados);
