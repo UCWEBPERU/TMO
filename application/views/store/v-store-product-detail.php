8cf9c88d70a61dda000d22032b77fc3095c8714c
Sheylavc <sheylavc@hotmail.com>
2016-03-23 11:45:35 -0500

    updates

--- application/views/store/v-store-product-detail.php
+++ application/views/store/v-store-product-detail.php
@@ -200,7 +200,7 @@
 
 
                 var request = $.ajax({
-                    url: <?php echo $modulo->base_url_store."/ajax/shopping/add"; ?>",
+                    url: "<?php echo $modulo->base_url_store."/ajax/shopping/add"; ?>",
                     method: "POST",
                     data: formData,
                     dataType: 'json',
