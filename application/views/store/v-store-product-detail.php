3c5fd67c1c798c41e20275d22a387043cb85ee6d
Sheylavc <sheylavc@hotmail.com>
2016-03-28 15:25:54 -0500

    updates

--- application/views/store/v-store-product-detail.php
+++ application/views/store/v-store-product-detail.php
@@ -144,6 +144,27 @@
         </div>
 
     </content>
+    <!-- Modal -->
+    <div id="modalAlert" class="modal fade" role="dialog">
+        <div class="modal-dialog">
+
+            <!-- Modal content-->
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    <h4 class="modal-title"></h4>
+                </div>
+                <div class="modal-body">
+                    <p></p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                </div>
+
+            </div>
+
+        </div>
+    </div>
     <footer>
         <div id="cart">
             <button id="shoppingcart"><h2>Add to Cart</h2></button>
@@ -253,15 +274,22 @@
                 request.done(function( response ) {
 
                     if (response.status) {
-                        alert(response.message);
+                        $('.modal-title').html("Add Item");
+                        $('.modal-body').html(response.message);
+                        $('#modalAlert').modal({show:true});
 
                     } else {
-                        alert(response.message);
+                        $('.modal-title').html("Add Item");
+                        $('.modal-body').html(response.message);
+                        $('#modalAlert').modal({show:true});
                     }
                 });
 
                 request.fail(function( jqXHR, textStatus ) {
-                    alert(textStatus);
+                   
+                    $('.modal-title').html("Add Item");
+                    $('.modal-body').html(textStatus);
+                    $('#modalAlert').modal({show:true});
                 });
 
 
