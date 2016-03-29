62e4cb3f3b6e729ab8486cb622c60a39a7d97103
Sheylavc <sheylavc@hotmail.com>
2016-03-29 16:00:11 -0500

    updates

--- application/views/store/v-store-home.php
+++ application/views/store/v-store-home.php
@@ -98,7 +98,6 @@
 			<a href="<?php echo $modulo->base_url_store; ?>"><img src="<?php echo PATH_RESOURCE_STORE; ?>images/home.png" class="images" alt="" /></a>
 			<h2><a href="<?php echo $modulo->base_url_store; ?>" onclick="">Products</a></h2>
 		</div>
-
 		<div class="boximage">
 			<a href="<?php echo $modulo->base_url_store; ?>/promotions"><img src="<?php echo PATH_RESOURCE_STORE; ?>images/sale.png" class="images" alt="" /></a>
 			<h1><a href="<?php echo $modulo->base_url_store; ?>/promotions" onclick="">Promotions</a></h1>
