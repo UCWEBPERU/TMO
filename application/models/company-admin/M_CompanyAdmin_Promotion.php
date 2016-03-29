edd3a4b00e453616cfec01867391eef00ed79314
Mick Heral V.E <mickhve@gmail.com>
2016-03-29 12:12:43 -0500

    updates

--- application/models/company-admin/M_CompanyAdmin_Promotion.php
+++ application/models/company-admin/M_CompanyAdmin_Promotion.php
@@ -1,5 +1,5 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-class M_CompanyAdmin_Offer extends CI_Model {
+class M_CompanyAdmin_Promotion extends CI_Model {
 
     public function __construct() {
         parent::__construct();
