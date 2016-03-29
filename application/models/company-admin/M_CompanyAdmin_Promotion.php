701e95a028fd7b775c0b48a34ffd151abfa0b301
Mick Heral V.E <mickhve@gmail.com>
2016-03-29 16:31:06 -0500

    updates

--- application/models/company-admin/M_CompanyAdmin_Promotion.php
+++ application/models/company-admin/M_CompanyAdmin_Promotion.php
@@ -54,6 +54,20 @@ class M_CompanyAdmin_Promotion extends CI_Model {
         return $query->num_rows();
     }
 
+    public function getProducts($id_empresa) {
+        $this->db->join('Sucursales', 'Sucursales.id_empresa = Empresa.id_empresa');
+        $this->db->join('Tienda', 'Tienda.id_tienda = Sucursales.id_tienda');
+        $this->db->join('Catalogo_Productos', 'Catalogo_Productos.id_tienda = Tienda.id_tienda');
+        $this->db->join('Producto', 'Producto.id_producto = Catalogo_Productos.id_producto');
+        $this->db->where('Empresa.id_empresa', $id_empresa);
+        $this->db->where('Producto.id_oferta', NULL);
+        $this->db->where('Empresa.estado', '1');
+        $this->db->where('Tienda.estado', '1');
+        $this->db->where('Producto.estado', '1');
+        $query = $this->db->get('Empresa');
+        return $query->result();
+    }
+
     public function getProductByPromotion($data_producto) {
         $this->db->select("Producto.id_producto,
 							Producto.nombre_producto,
