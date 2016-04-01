4e23d1d4dbdd616e685861c35df13ddadc9ea76d
Sheylavc <sheylavc@hotmail.com>
2016-04-01 09:49:45 -0500

    updates

--- application/views/store/v-store-home.php
+++ application/views/store/v-store-home.php
@@ -4,7 +4,7 @@
 	<meta charset="utf-8">
 	<title>TMO</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
-
+	<base href="<?php echo base_url();?>">
 	<!-- Link Swiper's CSS -->
 	<link rel="stylesheet" href="<?php echo PATH_RESOURCE_STORE; ?>css/swiper.min.css" />
 	<link rel="stylesheet" href="<?php echo PATH_RESOURCE_STORE; ?>css/main.css" />
