b153974d51b3e7b8ae6d28aacb3fe6cb06a2b7f9
Sheylavc <sheylavc@hotmail.com>
2016-04-01 11:25:29 -0500

    updates

--- application/models/store/M_Store.php
+++ application/models/store/M_Store.php
@@ -82,7 +82,6 @@ class M_Store extends CI_Model {
         $this->db->join('Tienda', 'Tienda.id_tienda = Catalogo_Productos.id_tienda');
         $this->db->join('Categoria_Productos', 'Categoria_Productos.id_categoria = Producto.id_categoria');
         $this->db->where('Categoria_Productos.id_empresa', $data["id_empresa"]);
-        $this->db->where('Categoria_Productos.id_categoria', $data["id_categoria"]);
         $this->db->where('Tienda.id_tienda', $data["id_tienda"]);
         $this->db->where('Categoria_Productos.estado', '1');
         $this->db->where('Producto.id_oferta', NULL);
@@ -151,6 +150,7 @@ class M_Store extends CI_Model {
         $this->db->join('Catalogo_Productos', 'Catalogo_Productos.id_producto = Producto.id_producto');
         $this->db->join('Oferta', 'Oferta.id_oferta = Producto.id_oferta');
         $this->db->join('Tienda', 'Tienda.id_tienda = Catalogo_Productos.id_tienda');
+        $this->db->join('Categoria_Productos', 'Categoria_Productos.id_categoria = Producto.id_categoria');
         $this->db->where('Categoria_Productos.id_empresa', $data["id_empresa"]);
         $this->db->where('Tienda.id_tienda', $data["id_tienda"]);
         $this->db->where('Categoria_Productos.estado', '1');
