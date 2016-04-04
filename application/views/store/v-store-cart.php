f6c837feb735799aad0ee93b5870abfc7ac3db85
Sheylavc <sheylavc@hotmail.com>
2016-04-04 15:27:38 -0500

    updates

--- application/views/store/v-store-cart.php
+++ application/views/store/v-store-cart.php
@@ -77,14 +77,13 @@ if ($cart = $this->cart->contents()): ?>
 
                                         <?php   $addtional += $modifier[3];
                                         $addtionals = $addtional  * $item['qty'];
-                                        echo $addtional ;
-                                        echo $addtionals ;
                                     endforeach; ?>
 
                                     <span>Subtotal :$ <?php echo number_format($item['subtotal'], 2) + $addtionals ?></span><br>
                                     <?php $grand_total +=  $item['subtotal'];
                                     $totaladditional += $addtionals;
-
+                                    echo $grand_total ;
+                                    echo $totaladditional ;
                                     ?>
 
                                 </div>
