bb133176437a094f8ebf63fbc626f75ace5dd6f7
Mick Heral V.E <mickhve@gmail.com>
2016-04-04 13:24:06 -0500

    updates

--- application/controllers/store/C_Store_Product.php
+++ application/controllers/store/C_Store_Product.php
@@ -43,7 +43,7 @@ class C_Store_Product extends CI_Controller {
                         "id_empresa"            => $this->uri->segment(2)
                     )
                 );
-                $modulo->url_button_back = generarUrlSubCategoria($modulo->base_url_store, $dataCategoria[0]->id_categoria, $dataCategoria[0]->id_categoria_superior);
+                $modulo->url_button_back = generarUrlSubCategoria($modulo->base_url_store, $dataCategoria[0]->id_categoria, $dataCategoria[0]->id_categoria_superior, "products");
                 $dataModifiers = $this->M_Store->getModifiers(
                     array(
                         "id_producto" => $producto->id_producto
@@ -89,7 +89,7 @@ class C_Store_Product extends CI_Controller {
                         "id_empresa"            => $this->uri->segment(2)
                     )
                 );
-                $modulo->url_button_back = generarUrlSubCategoria($modulo->base_url_store, $dataCategoria[0]->id_categoria, $dataCategoria[0]->id_categoria_superior);
+                $modulo->url_button_back = generarUrlSubCategoria($modulo->base_url_store, $dataCategoria[0]->id_categoria, $dataCategoria[0]->id_categoria_superior, "promotions");
                 $dataModifiers = $this->M_Store->getModifiers(
                     array(
                         "id_producto" => $producto->id_producto
@@ -103,7 +103,7 @@ class C_Store_Product extends CI_Controller {
 
         $data["modulo"] = $modulo;
 
-        $this->load->view('store/v-store-promotions-detail', $data);
+        $this->load->view('store/v-store-product-detail', $data);
     }
 
 }
