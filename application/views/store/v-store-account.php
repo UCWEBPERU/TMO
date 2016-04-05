060530b8a5010f9ed057daa06091358c1b9a33da
Mick Heral V.E <mickhve@gmail.com>
2016-04-05 10:26:50 -0500

    updates

--- application/views/store/v-store-account.php
+++ application/views/store/v-store-account.php
@@ -22,9 +22,31 @@
 <div id="panelAccount">
     <div class="item-menu-account">
         <a href="<?php echo $modulo->base_url_store; ?>">
-            <div class="text-menu">
-                MY ORDERS
+            <div class="text-menu"> MY ORDERS </div>
+            <div class="arrow-menu">
+                <img src="<?php echo PATH_RESOURCE_STORE; ?>img/icon_arrow.png">
             </div>
+        </a>
+    </div>
+    <div class="item-menu-account">
+        <a href="<?php echo $modulo->base_url_store; ?>">
+            <div class="text-menu"> MY EMAIL SETTINGS </div>
+            <div class="arrow-menu">
+                <img src="<?php echo PATH_RESOURCE_STORE; ?>img/icon_arrow.png">
+            </div>
+        </a>
+    </div>
+    <div class="item-menu-account">
+        <a href="<?php echo $modulo->base_url_store; ?>">
+            <div class="text-menu"> LEGAL INFORMATION </div>
+            <div class="arrow-menu">
+                <img src="<?php echo PATH_RESOURCE_STORE; ?>img/icon_arrow.png">
+            </div>
+        </a>
+    </div>
+    <div class="item-menu-account">
+        <a href="<?php echo $modulo->base_url_store; ?>">
+            <div class="text-menu"> HELP </div>
             <div class="arrow-menu">
                 <img src="<?php echo PATH_RESOURCE_STORE; ?>img/icon_arrow.png">
             </div>
