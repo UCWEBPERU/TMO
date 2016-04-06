5f0a662e2d8ad2659bad60f429680eaa2be7e94d
Mick Heral V.E <mickhve@gmail.com>
2016-04-06 11:02:20 -0500

    updates

--- application/views/store/v-store-sign-in.php
+++ application/views/store/v-store-sign-in.php
@@ -60,31 +60,31 @@
 <div id="menuApp">
     <div id="changeStyleProduct" class="menu-item">
         <a class="active" href="<?php echo $modulo->base_url_store; ?>">
-            <img src="<?php echo PATH_RESOURCE_STORE; ?>img/icon_menu_products.png">
+            <img src="<?php echo PATH_RESOURCE_STORE; ?>img/icon_menu_active_products.png">
             <div>PRODUCTS</div>
         </a>
     </div>
     <div class="menu-item">
         <a href="<?php echo $modulo->base_url_store; ?>/promotions">
-            <img src="<?php echo PATH_RESOURCE_STORE; ?>img/icon_menu_promotion.png">
+            <img src="<?php echo PATH_RESOURCE_STORE; ?>img/icon_menu_inactive_promotion.png">
             <div>PROMOTION</div>
         </a>
     </div>
     <div class="menu-item">
         <a href="<?php echo $modulo->base_url_store; ?>/search">
-            <img src="<?php echo PATH_RESOURCE_STORE; ?>img/icon_menu_search.png">
+            <img src="<?php echo PATH_RESOURCE_STORE; ?>img/icon_menu_inactive_search.png">
             <div>SEARCH</div>
         </a>
     </div>
     <div class="menu-item">
         <a href="<?php echo $modulo->base_url_store; ?>/account">
-            <img src="<?php echo PATH_RESOURCE_STORE; ?>img/icon_menu_account.png">
+            <img src="<?php echo PATH_RESOURCE_STORE; ?>img/icon_menu_active_account.png">
             <div>ACCOUNT</div>
         </a>
     </div>
     <div class="menu-item">
         <a href="<?php echo $modulo->base_url_store; ?>/cart">
-            <img src="<?php echo PATH_RESOURCE_STORE; ?>img/icon_menu_cart.png">
+            <img src="<?php echo PATH_RESOURCE_STORE; ?>img/icon_menu_inactive_cart.png">
             <div>CART</div>
         </a>
     </div>
