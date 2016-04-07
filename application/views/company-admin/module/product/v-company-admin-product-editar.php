4eb95c3895505d90877dcba692f006161b97bd12
Mick Heral V.E <mickhve@gmail.com>
2016-04-07 11:26:50 -0500

    updates

--- application/views/company-admin/module/product/v-company-admin-product-editar.php
+++ application/views/company-admin/module/product/v-company-admin-product-editar.php
@@ -243,7 +243,7 @@
             <div class="modal-content">
                 <div class="modal-header">
                     <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-                    <h4 class="modal-title">Responsive</h4>
+                    <h4 class="modal-title">Promocion</h4>
                 </div>
                 <div class="modal-body">
                     <div class="row">
