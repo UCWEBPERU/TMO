0156a326fb03d5d90850c60473f982513f4075fb
Sheylavc <sheylavc@hotmail.com>
2016-04-11 17:24:49 -0500

    updates

--- application/views/store/v-store-promotions.php
+++ application/views/store/v-store-promotions.php
@@ -42,7 +42,11 @@
         <?php for ( $c = 0; $c < sizeof($modulo->data_categorias); $c++) { ?>
             <div class="swiper-slide" >
                 <?php foreach ($modulo->data_productos as $producto) { ?>
-                <?php if($modulo->data_categorias[$c]->id_categoria == $producto->id_categoria ){?>
+                <?php if($modulo->data_categorias[$c]->id_categoria == $producto->id_categoria ){
+                        var_dump($modulo->data_categorias[$c]->id_categoria);
+                        var_dump(" - ");
+                        var_dump($producto->id_categoria);
+                        ?>
                     <div class="item-product-row">
                         <?php if ($modulo->tipo_sub_categorias == "products") { ?>
                         <a href="<?php echo $modulo->base_url_store."/products/".intval($producto->id_producto); ?>">
